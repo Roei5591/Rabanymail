8d0a7a1993e3519f41835187993b29d9952c68eb
Roei Rabany <roei5591@gmail.com>
2021-04-06 16:39:46 +0300

    changing cors origin

--- server.js
+++ server.js
@@ -32,8 +32,7 @@ app.use(cookieParser());
 app.use(
     cors({
       origin: "*", // <-- location of the react app were connecting to
-      AccessControlAllowOrigin: "*",
-      credentials: true,
+  
     })
   );
 
