f4301e3b5aed8517001176d9f5bb0d5c9be0c578
Roei Rabany <roei5591@gmail.com>
2021-04-06 17:26:09 +0300

    changing routs

--- routes/index.js
+++ routes/index.js
@@ -26,21 +26,21 @@ router.post('/register',
   router.get('/logout', userController.logout);
 
   router.post('/messages', catchErrors(messagesController.sendMessage));
-  router.get('/messages/inbox/' , catchErrors(messagesController.getInbox));
-  router.get('/messages/inbox/:userId' , catchErrors(messagesController.getInbox));
-
-  router.post('/messages/allmail/' , catchErrors(messagesController.getAllMail));
+  router.get('/messages/inbox' , catchErrors(messagesController.getInbox));
 
 
-  router.get('/messages/sent/' , catchErrors(messagesController.getSent));
-  router.get('/messages/sent/:userId' , catchErrors(messagesController.getSent));
+  router.post('/messages/allmail' , catchErrors(messagesController.getAllMail));
 
-  router.get('/messages/starred/' , catchErrors(messagesController.getStarred));
-  router.get('/messages/starred/:userId' , catchErrors(messagesController.getStarred));
 
-  router.post('/messages/starred/' , catchErrors(messagesController.toggleStarMassage));
-  router.post('/messages/markasread/' , catchErrors(messagesController.markAsRead));
-  router.post('/messages/istrash/' , catchErrors(messagesController.toggleIsTrash));
-  router.post('/messages/deleteMessages/' , catchErrors(messagesController.deleteMessages));
+  router.get('/messages/sent' , catchErrors(messagesController.getSent));
+
+
+  router.get('/messages/starred' , catchErrors(messagesController.getStarred));
+
+
+  router.post('/messages/starred' , catchErrors(messagesController.toggleStarMassage));
+  router.post('/messages/markasread' , catchErrors(messagesController.markAsRead));
+  router.post('/messages/istrash' , catchErrors(messagesController.toggleIsTrash));
+  router.post('/messages/deleteMessages' , catchErrors(messagesController.deleteMessages));
 
 module.exports = router;
